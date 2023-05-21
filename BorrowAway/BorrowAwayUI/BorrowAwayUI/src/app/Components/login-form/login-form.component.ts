1e4fd628de2e91deecd4eb16b538ef4795cec935
Andrei Beres <andrei.beres@yahoo.com>
2023-05-21 18:50:16 +0300

    linked register form to api

--- BorrowAway/BorrowAwayUI/BorrowAwayUI/src/app/Components/login-form/login-form.component.ts
+++ BorrowAway/BorrowAwayUI/BorrowAwayUI/src/app/Components/login-form/login-form.component.ts
@@ -11,21 +11,18 @@ export class LoginFormComponent {
   @ViewChild('emailTextBox', { static: false })
   emailTextBox?: DxTextBoxComponent;
 
-  @ViewChild('loginButton',{static:false})
-  loginButton?:DxButtonComponent;
-
   @Output() changeFormToRegisterEvent = new EventEmitter<void>();
 
   private _fieldsCompleted: number = 0;
   public userToLogin:LoginUser= {
-    email:'',
-    password:''
+    Email:'',
+    Password:''
   }
 
   public calculateCompletedFields(): void {
-    if (this.userToLogin.email != '' && this.userToLogin.password != '') {
+    if (this.userToLogin.Email != '' && this.userToLogin.Password != '') {
       this._fieldsCompleted = 2;
-    } else if (this.userToLogin.email == '' && this.userToLogin.password == '') {
+    } else if (this.userToLogin.Email == '' && this.userToLogin.Password == '') {
       this._fieldsCompleted = 0;
     } else {
       this._fieldsCompleted = 1;
@@ -44,7 +41,7 @@ export class LoginFormComponent {
     }
   }
   public loginClick():void{
-    console.log(this.userToLogin.email,this.userToLogin.password);
+    console.log(this.userToLogin.Email,this.userToLogin.Password);
     //TODO: Add Request to LOGIN
   }
   public registerClick():void{
--- BorrowAway/BorrowAwayUI/BorrowAwayUI/src/app/Components/register-form/register-form.component.ts
+++ BorrowAway/BorrowAwayUI/BorrowAwayUI/src/app/Components/register-form/register-form.component.ts
@@ -1,4 +1,7 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, EventEmitter, Output } from '@angular/core';
+import { RegisterUser } from 'src/app/Models/register-user';
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'register-form',
@@ -6,11 +9,23 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./register-form.component.scss']
 })
 export class RegisterFormComponent {
+  constructor(private _authService:AuthService){}
   @Output() changeFormToLoginEvent = new EventEmitter<void>();
 
-  public backToLoginClick():void{
-    console.log("Clicked button");
+  public userToRegister:RegisterUser ={
+    FirstName:'',
+    LastName:'',
+    Email:'',
+    Password:''
+  }
+  public registerButtonClick():void{
+    this._authService.registerUser(this.userToRegister).subscribe((response:any)=>{
+      console.log(response.body);
+    });
 
+  }
+
+  public backToLoginClick():void{
     this.changeFormToLoginEvent.emit();
   }
 }
