69a25739092cf8e04a759bddcbf37515ac11d1bc
Andrei Beres <andrei.beres@yahoo.com>
2023-06-03 20:57:56 +0300

    modified and added new models

---
+++ BorrowAway/BorrowAwayAPI/BorrowAwayAPI/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace BorrowAwayAPI.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Announcement> Announcements { get; set; }
+    }
+}
