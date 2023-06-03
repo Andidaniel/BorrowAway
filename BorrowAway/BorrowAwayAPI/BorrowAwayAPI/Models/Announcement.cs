521cdc00641ac2b644afd1613f3463ec425ef497
Andrei Beres <andrei.beres@yahoo.com>
2023-06-03 21:04:55 +0300

    updated announcement model

--- BorrowAway/BorrowAwayAPI/BorrowAwayAPI/Models/Announcement.cs
+++ BorrowAway/BorrowAwayAPI/BorrowAwayAPI/Models/Announcement.cs
@@ -3,7 +3,7 @@
     public class Announcement
     {
         public int Id { get; set; }
-        public int UserId { get; set; }
+        public Guid UserId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public int NumberOfImages { get; set; }
