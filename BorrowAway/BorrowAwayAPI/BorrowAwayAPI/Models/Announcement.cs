3d36c311b29f0af9c50854600781b1ea525443c1
Andrei Beres <andrei.beres@yahoo.com>
2023-06-03 21:12:50 +0300

    Revert "modified and added new models"
    This reverts commit 69a25739092cf8e04a759bddcbf37515ac11d1bc.

--- BorrowAway/BorrowAwayAPI/BorrowAwayAPI/Models/Announcement.cs
+++ BorrowAway/BorrowAwayAPI/BorrowAwayAPI/Models/Announcement.cs
@@ -7,15 +7,8 @@
         public string Title { get; set; }
         public string Description { get; set; }
         public int NumberOfImages { get; set; }
-        public float PricePerDay { get; set; }
-        public DateTime CreationDate { get; set; }
         public string ImagesDirectoryPath { get; set; }
-        public int CategoryId { get; set; }
-        public string  ContactMethod { get; set; }
-        public string Location { get; set; }
-
 
         public AppUser User { get; set; }
-        public Category Category { get; set; }
     }
 }
